69ce3c6a2f9e7610ef394d33516e58d258fd6e13
github username <you@example.com>
2020-02-27 20:41:41 -0500

    added authentication

--- boat-rental/config/routes.rb
+++ boat-rental/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   resources :rentals
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users 
+  get '/login', to: 'sessions#new'
+  get '/signup', to: 'users#new'
+  root to: 'rentals#index'
+  
 end
+
+
