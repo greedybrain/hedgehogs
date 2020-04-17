9a65531b9cda2af27e109833477edb99900149bf
ctdupuis <ctdupuis7@gmail.com>
2020-04-16 19:03:49 -0500

    challenge 2 done

---
+++ js-functions/index.js
@@ -0,0 +1,163 @@
+plants = [
+    {
+      "name": "Alder",
+      "species": "Alnus"
+    },
+    {
+      "name": "Black alder",
+      "species": "Alnus glutinosa, Ilex verticillata"
+    },
+    {
+      "name": "Common alder",
+      "species": "Alnus glutinosa"
+    },
+    {
+      "name": "False alder",
+      "species": "Ilex verticillata"
+    },
+    {
+      "name": "Gray alder",
+      "species": "Alnus incana"
+    },
+    {
+      "name": "Speckled alder",
+      "species": "Alnus incana"
+    },
+    {
+      "name": "Striped alder",
+      "species": "Ilex verticillata"
+    },
+    {
+      "name": "White alder",
+      "species": "Alnus incana, Ilex verticillata"
+    },
+    {
+      "name": "Almond",
+      "species": "Prunus amygdalus"
+    },
+    {
+      "name": "Tall ambrosia",
+      "species": "Ambrosia trifida"
+    },
+    {
+      "name": "Amy root",
+      "species": "Apocynum cannabinum"
+    },
+    {
+      "name": "Apple",
+      "species": "Malus domestica"
+    },
+    {
+      "name": "apple",
+      "species": "Maclura pomifera"
+    },
+    {
+      "name": "Apple of Sodom",
+      "species": "Solanum carolinense"
+    },
+    {
+      "name": "Apricot",
+      "species": "Prunus armeniaca"
+    },
+    {
+      "name": "Arfaj",
+      "species": "Rhanterium epapposum"
+    },
+    {
+      "name": "Arizona sycamore",
+      "species": "Platanus wrighitii"
+    },
+    {
+      "name": "Arrowwood",
+      "species": "Cornus florida"
+    },
+    {
+      "name": "Indian arrowwood",
+      "species": "Cornus florida"
+    },
+    {
+      "name": "Ash",
+      "species": "Fraxinus spp."
+    },
+    {
+      "name": "Black ash",
+      "species": "Acer negundo, Fraxinus nigra"
+    },
+    {
+      "name": "Blue ash",
+      "species": "Fraxinus pennsylvanica"
+    },
+    {
+      "name": "Cane ash",
+      "species": "Fraxinus americana"
+    },
+    {
+      "name": "Green ash",
+      "species": "Fraxinus pennsylvanica"
+    },
+    {
+      "name": "Maple ash",
+      "species": "Acer negundo"
+    },
+    {
+      "name": "Red ash",
+      "species": "Fraxinus pennsylvanica"
+    },
+    {
+      "name": "River ash",
+      "species": "Fraxinus pennsylvanica"
+    },
+    {
+      "name": "Swamp ash",
+      "species": "Fraxinus pennsylvanica"
+    },
+    {
+      "name": "White ash",
+      "species": "Fraxinus americana"
+    },
+    {
+      "name": "Water ash",
+      "species": "Acer negundo, Fraxinus pennsylvanica"
+    },
+    {
+      "name": "Azolla",
+      "species": "Azolla"
+    },
+    {
+      "name": "Carolina azolla",
+      "species": "Azolla caroliniana"
+    },
+    {
+      "name": "Bamboo",
+      "species": "Bambuseae"
+    }]
+
+// function titleCase(plants) {
+//     return plants.forEach(plant => {
+//         debugger
+//         plant.name.split(' ').forEach(word => {
+//             word.charAt(0).toUpperCase
+//         })
+//     })
+// }
+
+function toTitleCase(str) {
+    return str.replace(
+      /([^\W_]+[^\s-]*) */g,
+        function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1);
+        }
+    );
+  };
+  
+const titleCased = () => {
+    return plants.map(x => toTitleCase(x.name))
+}
+
+function filterBySpeciesPlants(plants, species) {
+    return plants.filter(function(plant) {
+        return plant.species === species
+    })
+}
+
+console.log(filterBySpeciesPlants(plants, 'Azolla'))
