a175f2f4f732c7779ab0a532d2e801f7fc5c7aac
chayadeaver <chayadeaver@gmail.com>
2020-06-11 20:34:03 -0400

    POD wk4

--- react-components/park-app/src/NewReservationForm.js
+++ react-components/park-app/src/NewReservationForm.js
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class NewReservationForm extends Component {
 
-  constructor(){
-    super()
-    this.state = {
-      name: "",
-      date: ""
-    }
-  }
-
-  handleChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
-
-    render() {
-        return(
-        <form onChange={this.handleChange} onSubmit={this.props.handleSubmit}>
-            Name: <input type='text' name='name'></input>
-            Date: <input type='text' name='date'></input>
-            <input type='submit'></input>
-        </form>
-        )
-    }
+const NewReservationForm = props => {
+  
+    return (
+    <form onChange={props.handleChange} onSubmit={props.handleSubmit}>
+        Name: <input type='text' name='name'></input>
+        Date: <input type='text' name='date'></input>
+        <input type='submit'></input>
+    </form>
+    )
 }
+
+export default NewReservationForm
+
+    
+
